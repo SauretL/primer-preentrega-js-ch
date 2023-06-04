9a095efd02d42cba9224258342210c4d72eec103
Sauret <sauret.lucas.ivan@gmail.com>
2023-06-04 18:24:18 -0300

    Programa finalizado con variables, ciclos, condicionales y funciones

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
-let manzana = 0
-let banana = 0
-let naranja = 0
-let producto
+function tienda() {
 
-while (producto != ("fin" || "salir")) {
-    producto = prompt("Ingrese los productos que desee consumir uno por uno. Cuando termine ingrese 'fin' o 'salir'\n1 Manzana\n1 Banana\n1 Naranja").toLowerCase().trim()
-    if (producto === "manzana") {
-        manzana++
-    }
-    if (producto === "banana") {
-        banana++
-    }
-    if (producto === "naranja") {
-        naranja++
-    }
+    let manzana = 0
+    let banana = 0
+    let naranja = 0
+    let producto
+    let seguro
+    do { 
+        manzana = 0
+        banana = 0
+        naranja = 0
+        producto = 0
+        while ((producto !== "fin") && (producto !== "salir")) {
+            producto = prompt("Ingrese los productos que desee consumir uno por uno. Cuando termine ingrese 'fin' o 'salir'\n1 Manzana\n1 Banana\n1 Naranja").toLowerCase().trim()
+            if (producto === "manzana") {
+                manzana++
+            }
+            if (producto === "banana") {
+                banana++
+            }
+            if (producto === "naranja") {
+                naranja++
+            }
+        }
+
+        function resultado(m, b, n) {
+            alert("Usted quiere " + m + " manzanas, " + b + " bananas y " + n + " naranjas.")
+        }
+
+        resultado(manzana, banana, naranja)
+       
+        do {
+            seguro = prompt("¿Usted esta seguro/a? Si/No").toLowerCase().trim()
+        } while (seguro != "no" && seguro != "si");
+       
+    
+} while (seguro != "si");
+    
 }
 
-console.log(producto)
-console.log("El numero de manzanas es " + manzana)
-console.log("El numero de bananas es " + banana)
-console.log("El numero de naranjas es " + naranja)
+tienda()
+
+alert("Fin del programa")
